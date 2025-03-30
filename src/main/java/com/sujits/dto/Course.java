639efc34fb29b52355f8eb15911bfdf561739d06
Levi Ackerman <sujitsugathan2002@gmail.com>
2025-03-30 13:17:08 +0530

    updated to github
    updated to github for github actions

---
+++ src/main/java/com/sujits/dto/Course.java
@@ -0,0 +1,16 @@
+package com.sujits.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    private int id;
+    private String name;
+    private double price;
+}
---
+++ src/test/java/com/sujits/AwsDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sujits;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
